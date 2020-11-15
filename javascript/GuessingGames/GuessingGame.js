ebb86d95cb9cc593fed81b947a05b66d77723581
Xiyin Chu <71158245+SheenC@users.noreply.github.com>
2020-11-15 21:33:03 +0800

    Add files via upload

---
+++ javascript/GuessingGames/GuessingGame.js
@@ -0,0 +1,28 @@
+
+
+let maximum = parseInt(prompt("Enter the maximum number!"));
+
+while (!maximum) {
+    maximum = parseInt(prompt("Emter a valid number!"))
+}
+
+const targetNum = Math.floor(Math.random() * maximum ) + 1;
+let attempts = 1;
+
+let guess = prompt("Enter your first guess!");
+while (parseInt(guess) !== targetNum) {
+    if (guess === 'q') break;
+    attempts++;
+    if (guess > targetNum) {
+        guess = prompt("Too high! Enter a new guess:");
+    } else {
+        guess = prompt("Too low! Enter a new guess:");
+    }
+}
+
+if (guess === 'q') {
+    console.log("OK, YOU QUIT!");
+} else {
+    console.log("CONGRATS YOU WIN!");
+    console.log(`YOU GOT IT! It took you ${attempts} guesses`);
+}
